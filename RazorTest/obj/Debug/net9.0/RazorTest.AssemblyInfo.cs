ceec6fda286ae8ce4e3574802c90f00625a10cab
Graniten40 <graniten40@gmail.com>
2025-11-30 13:36:48 +0100

    Update

--- RazorTest/obj/Debug/net9.0/RazorTest.AssemblyInfo.cs
+++ RazorTest/obj/Debug/net9.0/RazorTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317fd631f2c3a73cdc5bbdbb6f512c846c76710a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb028f32442d4f57fa8b1653c47903f0642769c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
